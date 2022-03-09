b3b3691a0d46a3af4d7ecd2d28dcdf55e4f38880
srikanthpragada <srikanthpragada@gmail.com>
2022-03-09 19:55:47 +0530

    09-MAR

---
+++ demo/assignments/count_factors.py
@@ -0,0 +1,9 @@
+# Count Factors for the given number
+num = int(input("Enter a number : "))
+
+count = 0
+for n in range(2, num // 2 + 1):
+    if num % n == 0:
+         count += 1
+
+print(f"No. of factors = {count}")
---
+++ demo/assignments/print_reverse.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number : "))
+
+for n in range(num, 0, -1):
+    print(n)
+
+# while num >= 1:
+#     print(num)
+#     num -= 1
